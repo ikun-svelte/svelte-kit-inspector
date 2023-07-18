a2e18cb6ad6c917a2e6106a9442200f254bb3531
baiwusanyu-c <740132583@qq.com>
2023-07-18 17:39:31 +0800

    style: Optimize button style

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,13 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 import svelteKitInspector from 'svelte-kit-inspector'
-import { svelteInspector } from '@sveltejs/vite-plugin-svelte-inspector';
 export default defineConfig({
   plugins: [
     svelteKitInspector(),
-    /*svelteInspector({
-      /!* plugin options *!/
-    }),*/
     sveltekit(),
   ],
 })
